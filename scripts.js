a265ef5ef1eb0338dd0f0aa7887d9e571fca9ad9
AAgência Digital <aagenciati@gmail.com>
2021-07-21 18:02:41 -0300

    carossel e botao de whats

--- scripts.js
+++ scripts.js
@@ -3,3 +3,28 @@ function scrollSmoothTo(elementId) {
   element.scrollIntoView({ block: 'start',  behavior: 'smooth' });
   document.getElementById("checkbox-menu").checked = false;
 }
+
+window.onload = function(){
+  var slides = document.getElementsByClassName('carousel-item'),
+      addActive = function(slide) {slide.classList.add('active')},
+      removeActive = function(slide) {slide.classList.remove('active')};
+  addActive(slides[0]);
+  
+  setInterval(function (){
+    for (var i = 0; i < slides.length; i++){
+      if (i + 1 == slides.length) {
+        addActive(slides[0]);
+        slides[0].style.zIndex = 100;
+        setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+        break;
+      }
+      if (slides[i].classList.contains('active')) { 
+        slides[i].removeAttribute('style');
+        setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
+        addActive(slides[i + 1]);
+        break;
+      }
+    } 
+  }, 6000);
+}
+  
